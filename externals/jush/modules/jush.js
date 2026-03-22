b2f7f9c5bf0ec6029f418481088d7e19f0b6d9ed
Максим <sailormax@live.com>
2026-03-22 21:25:10 +0300

    update jush

--- externals/jush/modules/jush.js
+++ externals/jush/modules/jush.js
@@ -44,7 +44,7 @@ var jush = {
 	highlight: function (language, text) {
 		this.last_tag = '';
 		this.last_class = '';
-		return this.highlight_states([ language ], text.replace(/\r\n?/g, '\n'), !/^(htm|tag|xml|txt)$/.test(language))[0];
+		return '<span class="jush">' + this.highlight_states([ language ], text.replace(/\r\n?/g, '\n'), !/^(htm|tag|xml|txt)$/.test(language))[0] + '</span>';
 	},
 
 	/** Highlight html
@@ -186,7 +186,7 @@ var jush = {
 			for (var url in this.custom_links[state]) {
 				s = s.replace(this.custom_links[state][url], function (str) {
 					var offset = arguments[arguments.length - 2];
-					if (/<[^>]*$/.test(s.substr(0, offset))) {
+					if (/<[^>]*$/.test(s.substr(0, offset)) || /^[^<]*<\/a>/.test(s.substr(offset))) {
 						return str; // don't create links inside tags
 					}
 					return '<a href="' + jush.htmlspecialchars_quo(url.replace('$&', encodeURIComponent(str))) + '" class="jush-custom">' + str + '</a>' // not create_link() - ignores create_links
